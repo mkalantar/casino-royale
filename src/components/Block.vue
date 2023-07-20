7f5c7426c720cbf4bc5a4cfbd0d00bccd80184ce
Morteza Kalantar <m.kalantar@dsi.ir>
2023-07-21 02:53:30 +0330

    add base block component

---
+++ src/components/Block.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="block">
+    <component :is="cmp" :size="size" />
+  </div>
+</template>
+<script setup>
+import { computed } from "vue";
+import Cherry from "./Cherry.vue";
+import Lemon from "./Lemon.vue";
+import Watermelon from "./Watermelon.vue";
+import Orange from "./OrangeIcon.vue";
+import Spinner from "./Spinner.vue";
+const props = defineProps({
+  symbol: String,
+  size: {
+    type: Number,
+    default: 50,
+  },
+});
+
+const cmp = computed(() => {
+  switch (props.symbol.toLowerCase()) {
+    case "cherry":
+      return Cherry;
+    case "watermelon":
+      return Watermelon;
+    case "orange":
+      return Orange;
+    case "lemon":
+      return Lemon;
+    case "spinner":
+      return Spinner;
+  }
+});
+</script>
+<style lang="scss" scoped>
+.block {
+  width: 100px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+}
+</style>
